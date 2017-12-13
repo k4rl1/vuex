103b44f1a1423412bf60f8120014f9b64c93e4e7
Karl Zierhofer <karl.zierhofer@laola1.at>
2017-12-13 20:57:24 +0100

    add actions and input field

--- resources/assets/js/components/Counter.vue
+++ resources/assets/js/components/Counter.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container">
-        {{count}}
+        {{count}} the square root of this number is {{sqrt}}
+        <p>
+            <input type="text" :value="msg" @input="updateMsg">{{msg}}
+        </p>
         <p>
             <button @click="increment">+1</button>
             <button @click="decrement">-1</button>
@@ -11,11 +14,27 @@
 <script>
     import {mapState, mapMutations} from 'vuex';
     export default {
-        computed: mapState(['count']),
-        methods: _.extendOwn(mapMutations(['increment', 'decrement']), {
-            custom() {
-                //bla
+        computed: {
+            count() {
+                return this.$store.state.count;
+            },
+            sqrt() {
+                return this.$store.getters.sqrt;
+            },
+            msg() {
+                return this.$store.state.msg;
             }
-        })
+        },
+        methods: {
+            increment() {
+                this.$store.dispatch('increment');
+            },
+            decrement() {
+                this.$store.dispatch('decrement');
+            },
+            updateMsg(e) {
+                this.$store.dispatch('increment', e.target.value);
+            }
+        }
     }
 </script>
--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -1,6 +1,23 @@
 export default {
     state: {
-        count: 0
+        count: 0,
+        msg: "hello you"
+    },
+    getters: {
+        sqrt(state) {
+            return Math.sqrt(state.count);
+        }
+    },
+    actions: {
+        increment({commit, state}, text) {
+            commit('increment');
+            commit('msg', text);
+        },
+        decrement(context) {
+            setTimeout(() => {
+                context.commit('decrement');
+            }, 2000);
+        }
     },
     mutations: {
         increment(state) {
@@ -8,6 +25,9 @@ export default {
         },
         decrement(state) {
             state.count--;
+        },
+        msg(state, msg) {
+            state.msg = msg;
         }
     }
 }
